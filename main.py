22d10cc6c74cd14eae6f1ddefc9e7b63a9ddc4e2
sctakashi1 <81713596+sctakashi1@users.noreply.github.com>
2021-04-01 01:29:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ download_dict = {}
 
 
 def start_bot():
-    bot = telebot.TeleBot(config.config('bot_token'), threaded=True, num_threads=300)
+    bot = telebot.TeleBot(config.config('bot_token'),num_threads=300)
 
     # Command start
     @bot.message_handler(commands=['start'])
